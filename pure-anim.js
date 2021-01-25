3a85f15f913327c8f6c49f77144689eba72c8382
EneroPL <lankowicz.pl@gmail.com>
2021-01-25 13:26:29 +0300

    Added validation & removed calling class

--- pure-anim.js
+++ pure-anim.js
@@ -1,6 +1,16 @@
-class AnimPlus {
-  constructor(elems) {
-    this.elems = elems;
+// Pure Animation Errors
+class PureAnimError extends Error {}
+class PureAnimSyntaxError extends PureAnimError {
+  constructor(message, errorTag, errorClasses) {
+    super(message);
+    this.name = 'PureAnim SyntaxError';
+    this.errorTag = errorTag;
+    this.errorClasses = errorClasses;
+  }
+}
+// Pure Animation
+class PureAnim {
+  constructor() {
     this.defaultAnimationClasses = ['fade-in', 'from-left', 'from-top', 'from-right', 'from-bottom'];
     this.observeOptions = {
       root: null,
@@ -11,6 +21,10 @@ class AnimPlus {
   }
 
   initialize() {
+    let targetClasses = [...this.defaultAnimationClasses];
+    targetClasses = targetClasses.map(item => `.${item}`);
+    this.elems = document.querySelectorAll(targetClasses.join(','));
+
     this.elems.forEach((item, i) => {
       let observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
@@ -28,11 +42,21 @@ class AnimPlus {
   }
 
   animHandler(elem) {
-    let classes = elem.className;
-    classes = classes.split(' ').filter(item => this.defaultAnimationClasses.includes(item));
-    
-    classes.forEach(classItem => {
-      elem.classList.add(`${classItem}_active`);
-    });
+    try {
+      let classes = elem.className;
+      classes = classes.split(' ').filter(item => this.defaultAnimationClasses.includes(item));
+
+      if(classes.length > 1) {
+        throw new PureAnimSyntaxError('You\'r HTML-element must have 1 attribute only! You\'r wrote: ', elem.tagName,classes.join(', '));
+      } else {
+        classes.forEach(classItem => {
+          elem.classList.add(`${classItem}_active`);
+        });
+      }
+    } catch(err) {
+      console.error(`${err.name}: ${err.message}${err.errorClasses} in "${err.errorTag}" element!`);
+    }
   }
 }
+
+new PureAnim();
